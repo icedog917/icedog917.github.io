ccf3d1b03dd55e6de057c92fea1a2e0cd8c62199
icedog917 <Justin@teambenning.com>
2023-02-14 16:06:47 -0600

    Add Miles

--- Countdown.js
+++ Countdown.js
@@ -1,5 +1,8 @@
 window.onload = () => {
+    countdown();
+    miles();
     setInterval(countdown, 1000);
+    setInterval(miles, 1000000);
 };
 const countdown = () => {
     const countDownDate = new Date("Feb 25, 2023 21:00:00").getTime();
@@ -13,4 +16,16 @@ const countdown = () => {
 
     const header = document.getElementById("countdown");
     header.innerHTML = days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds";
+};
+
+const miles = () => {
+    const countDownDate = new Date("Feb 6, 2023 01:00:00").getTime();
+    const now = new Date().getTime();
+    const timeLeft = now - countDownDate;
+
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const miles = Math.floor(days*(10000/365)) + 12;
+
+    const header = document.getElementById("miles");
+    header.innerHTML = miles + " miles";
 };
